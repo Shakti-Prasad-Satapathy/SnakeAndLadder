eb640d472843b22bd3b52b9a51423e722a9306c1
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-24 14:54:45 +0530

    added logic for continue the move upto the user 1 reached to position 100 by using while loop

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -x
 
 user1=0
-die_value=$(( (RANDOM % 6) +1 ))
+while [[ $user1 -lt 100 ]]
+do
+	die_value=$(( (RANDOM % 6) +1 ))
+	option=$(( (RANDOM % 3) +1 ))
 
-option=$(( (RANDOM % 3) +1 ))
-
-if [[ $option -eq 1 ]]
-then
-	user1=$(( user1 + die_value ))
-elif [[ $option -eq 2 ]]
-then
-	user1=$(( user1 - die_value ))
-	if [[ $user1 -lt 0 ]]
+	if [[ $option -eq 1 ]]
 	then
-		user1=0
+		user1=$(( user1 + die_value ))
+	elif [[ $option -eq 2 ]]
+	then
+		user1=$(( user1 - die_value ))
+		if [[ $user1 -lt 0 ]]
+		then
+			user1=0
+		fi
 	fi
-fi
+done
