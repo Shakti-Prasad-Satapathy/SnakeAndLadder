748938c6e2666f286e1c775c29a7b75457ce7576
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-24 15:19:34 +0530

    added logic and condetions for 2 players

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
 
 user1=0
-count=0
-while [[ $user1 -lt 100 ]]
+user2=0
+count=1
+while [[ $user1 -lt 100 || $user2 -lt 100 ]]
 do
 	die_value=$(( (RANDOM % 6) +1 ))
 	option=$(( (RANDOM % 3) +1 ))
 
-	if [[ $option -eq 1 ]]
+	if [[ $option -eq 1 && $(( count % 2 )) -eq 0 ]]
 	then
 		user1=$(( user1 + die_value ))
 		if [[ $user1 -gt 100 ]]
@@ -16,17 +17,38 @@ do
 		elif [[ $user1 -eq 100 ]]
 		then
 			echo Congrats User 1... You Win
-			echo Total $(( count )) number of time the dice was played
+			echo Total $(( count - 1 )) number of time the dice was played
 			exit
 		fi
-	elif [[ $option -eq 2 ]]
+	elif [[ $option -eq 2 && $(( count % 2 )) -eq 0 ]]
 	then
 		user1=$(( user1 - die_value ))
 		if [[ $user1 -lt 0 ]]
 		then
 			user1=0
 		fi
-	fi
+	elif [[ $option -eq 1 && $(( count % 2 )) -ne 0 ]]
+   then
+      user2=$(( user2 + die_value ))
+      if [[ $user1 -gt 100 ]]
+      then
+         user2=$(( user2 - die_value ))
+      elif [[ $user2 -eq 100 ]]
+      then
+         echo Congrats User 2... You Win
+			echo Total $(( count - 1 )) number of time the dice was played
+         exit
+      fi
+   elif [[ $option -eq 2 && $(( count % 2 )) -ne 0 ]]
+   then
+      user2=$(( user2 - die_value ))
+      if [[ $user2 -lt 0 ]]
+      then
+         user2=0
+      fi
+   fi
 	count=$(( count + 1 ))
+	user2array[count]=$user2
 	user1array[count]=$user1
 done
+
